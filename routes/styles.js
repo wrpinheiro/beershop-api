72b9b51a3780855e619efe0aaf8324414db31d39
Wellington Pinheiro <Wellington Ricardo Pinheiro wellington.pinheiro@gmail.com>
2016-03-02 23:59:18 -0300

    Add skeleton for styles CRUD operations.

--- routes/styles.js
+++ routes/styles.js
@@ -1,9 +1,43 @@
 module.exports = app => {
   const Styles = app.db.models.Styles;
 
-  app.get("/styles", (req, res) => {
-    Styles.findAll({}).then(styles => {
-      res.json({styles: styles});
+  app.route("/styles")
+    .all((req, res, next) => {
+      if (req.body) {
+        delete req.body.id;
+      }
+
+      next();
+    })
+    .get((req, res) => {
+      Styles.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: "erro"});
+        });
+    })
+    .post((req, res) => {
+      Styles.create(req.body)
+      .then(result => res.json(result))
+      .catch(error => {
+        res.status(412).json({msg: "erro criando estilo"});
+      });
     });
-  });
+  app.route("/styles/:id")
+    .all((req, res) => {
+      if (req.body) {
+        delete req.body.id;
+      }
+
+      next();
+    })
+    .get((req, res) => {
+
+    })
+    .put((req, res) => {
+
+    })
+    .delete((req, res) => {
+
+    });
 };
